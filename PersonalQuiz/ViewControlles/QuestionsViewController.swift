0e1b9ae8e9f068303ec60b0ba70a6f9ff3645c0b
Aleksei Podlevskii <alexp999@mail.ru>
2020-06-03 13:48:12 +0500

    Home work ready.

--- PersonalQuiz/ViewControlles/QuestionsViewController.swift
+++ PersonalQuiz/ViewControlles/QuestionsViewController.swift
@@ -153,15 +153,13 @@ extension QuestionsViewController {
         }
     }
     
-    
-    /*
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+      if segue.identifier == "resultSegue" {
+        let destinationVC = segue.destination as! ResultsViewController
+        destinationVC.answers = answersChoosen
+      }
     }
-    */
 }
 
 
--- PersonalQuiz/ViewControlles/ResultsViewController.swift
+++ PersonalQuiz/ViewControlles/ResultsViewController.swift
@@ -9,20 +9,69 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+  
+  // 2. Определить наиболее часто встречающийся тип животного
+  
+  @IBOutlet weak var animalLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  var answers: [Answer]!
+  
+  private var animalTypeCounts = [AnimalType.dog: 0,
+                     AnimalType.cat: 0,
+                     AnimalType.rabbit: 0,
+                     AnimalType.turtle: 0]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    // 1. Передать массив с ответами на этот экран
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результаты на экране
-    // 4. Избавится от кнопки back
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    navigationItem.hidesBackButton = true
+    
+    calculateAnimalTypes()
+    
+    let result = answerDefinition()
+    
+    animalLabel.text = "Вы - \(result.rawValue)"
+    descriptionLabel.text = result.definition
+  }
+  
+  private func answerDefinition() -> AnimalType {
+    var animal = AnimalType.dog
+    var animalCount = 0
+    
+    for pet in animalTypeCounts {
+      if pet.value > animalCount {
+        animal = pet.key
+        animalCount = pet.value
+      }
     }
     
-    deinit {
-        print("ResultsViewController has been dealocated")
+    return animal
+  }
+  
+  private func calculateAnimalTypes() {
+    for answer in answers {
+      switch answer.type {
+      case .dog:
+        increaseCount(for: .dog)
+      case .cat:
+        increaseCount(for: .cat)
+      case .rabbit:
+        increaseCount(for: .rabbit)
+      case .turtle:
+        increaseCount(for: .turtle)
+      }
     }
-
+  }
+  
+  private func increaseCount(for animal: AnimalType) {
+    if animalTypeCounts[animal] != nil {
+      animalTypeCounts[animal]! += 1
+    }
+  }
+  
+  deinit {
+    print("ResultsViewController has been dealocated")
+  }
+  
 }
